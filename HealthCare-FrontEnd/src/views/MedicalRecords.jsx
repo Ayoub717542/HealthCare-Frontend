a8ec00b2bfc00930ee6898ad27e565135019ebfb
Ayoub Hadi <AyoubHadi856@gmail.com>
2026-07-22 10:02:01 +0100

    update the Records

--- HealthCare-FrontEnd/src/views/MedicalRecords.jsx
+++ HealthCare-FrontEnd/src/views/MedicalRecords.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import api from "../service/api";
 
 function MedicalRecords(){
-  console.log("MedicalRecords component rendered");
 
   const [recordes, setRecordes] = useState([]);
+
    function fetchMedicalRecords() {
     api.get("/DossierMedical/getAllDossierMedical")
         .then((response) => {
@@ -15,12 +15,14 @@ function MedicalRecords(){
         });
 }
     useEffect(()=>{
-         console.log("Fetching records...");
         fetchMedicalRecords()
     },[]);
 
     return(
         <>
+        <div className="recordes">
+
+       
        <div className="table-header">
        <h2>Medical Recoreds</h2>
        <button>add An Record</button>
@@ -33,18 +35,17 @@ function MedicalRecords(){
                 <th>observations</th>
                 <th>dateCreation</th>
                 <th>patient</th>
+                <th>Action</th>
             </tr>
             </thead>
                 <tbody>
                     {recordes.map((record)=> (
                     <tr key={record.id}>
                         <td>{record.id}</td>
-                    <td>{record.diagnostic}</td>
-                       <td>{record.observations}</td>
-                          <td>{record.dateCreation}</td>
-                           <td>
-    {record.patient?.nom} {record.patient?.prenom}
-</td>
+                        <td>{record.diagnostic}</td>
+                        <td>{record.observations}</td>
+                        <td>{record.dateCreation}</td>
+                        <td>{record.patient?.nom} {record.patient?.prenom}</td>
                     <td>
                         <button><i className="fa-solid fa-pen"></i>Edit</button>
                          <button><i className="fa-solid fa-trash"></i> Delete</button>
@@ -57,6 +58,7 @@ function MedicalRecords(){
                 </tbody>
    
        </table>
+        </div>
 
 </>
     )
